e4e24435235fc300cad725b42f98c03f92113e58
xav35510 <xav2graeve@gmail.com>
2025-01-29 10:46:09 +0100

    mega test

--- Assets/test.cs
+++ Assets/test.cs
@@ -5,7 +5,7 @@ public class test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        print("hahah");
     }
 
     // Update is called once per frame
